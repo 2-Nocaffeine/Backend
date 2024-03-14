998bd3b824547765546c4c7b9778cca8babe81d5
Kim seonuk <kswms403@naver.com>
2024-03-14 14:19:09 +0900

    feat - 특정 상품 조회

--- src/main/java/com/nocaffeine/ssgclone/product/domain/Product.java
+++ src/main/java/com/nocaffeine/ssgclone/product/domain/Product.java
@@ -2,9 +2,12 @@ package com.nocaffeine.ssgclone.product.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class Product {
+
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
---
+++ src/main/java/com/nocaffeine/ssgclone/product/repository/ViewHistoryRepository.java
@@ -0,0 +1,9 @@
+package com.nocaffeine.ssgclone.product.repository;
+
+import com.nocaffeine.ssgclone.product.domain.ViewHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ViewHistoryRepository extends JpaRepository<ViewHistory, Long> {
+}
