d87e3061b6f2cfe938ad88f5ae65eee8204c7876
Doyeon <ansehdus7@gmail.com>
2024-03-15 09:48:39 +0900

    dev create

--- src/main/java/com/nocaffeine/ssgclone/order/domain/Orders.java
+++ src/main/java/com/nocaffeine/ssgclone/order/domain/Orders.java
@@ -1,5 +1,6 @@
 package com.nocaffeine.ssgclone.order.domain;
 
+import com.nocaffeine.ssgclone.common.BaseTimeEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 
