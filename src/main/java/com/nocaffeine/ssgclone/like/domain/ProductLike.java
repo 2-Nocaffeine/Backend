146eca0eb812f4f525607b8bb478fbcbc4bc8fb2
Doyeon <ansehdus7@gmail.com>
2024-03-21 20:21:17 +0900

    feat : Product 누락된 컬럼 추가

--- src/main/java/com/nocaffeine/ssgclone/like/domain/ProductLike.java
+++ src/main/java/com/nocaffeine/ssgclone/like/domain/ProductLike.java
@@ -1,10 +1,17 @@
 package com.nocaffeine.ssgclone.like.domain;
 
 import com.nocaffeine.ssgclone.member.domain.Member;
+import com.nocaffeine.ssgclone.product.domain.Product;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class ProductLike {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -13,6 +20,18 @@ public class ProductLike {
     @NotNull
     private Member member;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @NotNull
+    private Product product;
+
     @Column(name ="folder_id")
     private Long folder;
+
+
+    @Builder
+    public ProductLike(Member member, Product product, Long folder) {
+        this.member = member;
+        this.product = product;
+        this.folder = folder;
+    }
 }
