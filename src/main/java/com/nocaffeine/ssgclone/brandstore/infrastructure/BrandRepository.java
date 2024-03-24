4fc0d40a80445eefcf430cad2c24e2d2e2bffc49
Doyeon <ansehdus7@gmail.com>
2024-03-24 15:36:26 +0900

    feat : BrandRepository 추가

---
+++ src/main/java/com/nocaffeine/ssgclone/brandstore/infrastructure/BrandRepository.java
@@ -0,0 +1,9 @@
+package com.nocaffeine.ssgclone.brandstore.infrastructure;
+
+import com.nocaffeine.ssgclone.brandstore.domain.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+}
