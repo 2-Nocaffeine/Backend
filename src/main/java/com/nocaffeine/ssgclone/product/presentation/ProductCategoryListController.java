228c7d8499b7399c33c1f317c773604211ded1e4
22gamin <gamin3182@gmail.com>
2024-03-27 15:46:38 +0900

    before pull

--- src/main/java/com/nocaffeine/ssgclone/product/presentation/ProductCategoryListController.java
+++ src/main/java/com/nocaffeine/ssgclone/product/presentation/ProductCategoryListController.java
@@ -13,34 +13,34 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/product/")
+@RequestMapping("/api/v1/product")
 public class ProductCategoryListController {
 
     private final ProductCategoryListService productCategoryListService;
 
     //대분류별 상품 조회
-    @GetMapping("/large/{largeId}/")
+    @GetMapping("/large/{largeId}")
     public CommonResponse<List<ProductIdListResponse>> productIdWithLargeCategoryList(@PathVariable("largeId") Long largeId){
         List<ProductIdListResponse> productIdListResponseList = productCategoryListService.getProductIdListWithLargeCategory(largeId);
         return CommonResponse.success("해당 카테고리의 productIdList를 성공적으로 찾았습니다.",productIdListResponseList);
     }
 
     //중분류별 상품 조회
-    @GetMapping("/medium/{mediumId}/")
+    @GetMapping("/medium/{mediumId}")
     public CommonResponse<List<ProductIdListResponse>> productIdWithMediumCategoryList(@PathVariable("mediumId") Long mediumId){
         List<ProductIdListResponse> productIdListResponseList = productCategoryListService.getProductIdListWithMediumCategory(mediumId);
         return CommonResponse.success("해당 카테고리의 productIdList를 성공적으로 찾았습니다.",productIdListResponseList);
     }
 
     //소분류별 상품 조회
-    @GetMapping("/small/{smallId}/")
+    @GetMapping("/small/{smallId}")
     public CommonResponse<List<ProductIdListResponse>> productIdWithSmallCategoryList(@PathVariable("smallId") Long smallId){
         List<ProductIdListResponse> productIdListResponseList = productCategoryListService.getProductIdListWithSmallCategory(smallId);
         return CommonResponse.success("해당 카테고리의 productIdList를 성공적으로 찾았습니다.",productIdListResponseList);
     }
 
     //소소분류별 상품 조회
-    @GetMapping("/tiny/{tinyId}/")
+    @GetMapping("/tiny/{tinyId}")
     public CommonResponse<List<ProductIdListResponse>> productIdWithTinyCategoryList(@PathVariable("tinyId") Long tinyId){
         List<ProductIdListResponse> productIdListResponseList = productCategoryListService.getProductIdListWithTinyCategory(tinyId);
         return CommonResponse.success("해당 카테고리의 productIdList를 성공적으로 찾았습니다.",productIdListResponseList);
