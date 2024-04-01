75295614e4259b1168e2bd8a8c753b61fa17d283
Doyeon <ansehdus7@gmail.com>
2024-04-02 01:05:15 +0900

    chore : 스웨거 작성

--- src/main/java/com/nocaffeine/ssgclone/member/presentation/AuthController.java
+++ src/main/java/com/nocaffeine/ssgclone/member/presentation/AuthController.java
@@ -11,6 +11,7 @@ import com.nocaffeine.ssgclone.member.vo.request.AuthEmailRequestVo;
 import com.nocaffeine.ssgclone.member.vo.request.SnsMemberAddRequestVo;
 import com.nocaffeine.ssgclone.member.vo.request.SnsMemberLoginRequestVo;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -20,19 +21,20 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@Tag(name = "Auth", description = "인증")
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
     private final AuthService authService;
 
-    @Operation(summary = "소셜 회원가입", description = "소셜 회원가입", tags = {"Auth"})
+    @Operation(summary = "소셜 회원가입", description = "소셜 회원가입")
     @PostMapping("/sns-join")
     public CommonResponse<String> snsCreateMember(@RequestBody SnsMemberAddRequestVo snsMemberAddRequestVo) {
         authService.snsAddMember(SnsMemberAddRequestVo.voToDto(snsMemberAddRequestVo));
         return CommonResponse.success("소셜 회원가입 성공");
     }
 
-    @Operation(summary = "소셜 로그인", description = "소셜 로그인", tags = {"Auth"})
+    @Operation(summary = "소셜 로그인", description = "소셜 로그인")
     @PostMapping("/sns-login")
     public ResponseEntity<CommonResponse<TokenResponseDto>> snsLogin(@RequestBody SnsMemberLoginRequestVo snsMemberLoginRequestVo) {
         TokenResponseDto tokenResponseDto = authService.snsLogin(SnsMemberLoginRequestDto.voToDto(snsMemberLoginRequestVo));
@@ -42,15 +44,15 @@ public class AuthController {
     }
 
 
-    @Operation(summary = "이메일 인증코드 발송", description = "이메일 인증코드 발송", tags = {"Auth"})
-    @PostMapping("/authentications/email")
+    @Operation(summary = "이메일 인증코드 발송", description = "이메일 인증코드 발송")
+    @PostMapping("/email")
     public CommonResponse<String> emailAuth(@RequestBody AuthEmailRequestVo authEmailRequestVo) {
         authService.emailAuth(AuthEmailRequestDto.voToDto(authEmailRequestVo));
         return CommonResponse.success("이메일 인증코드 발송 성공");
     }
 
-    @Operation(summary = "이메일 인증코드 확인", description = "이메일 인증코드 확인", tags = {"Auth"})
-    @PostMapping("/authentications/email/check")
+    @Operation(summary = "이메일 인증코드 확인", description = "이메일 인증코드 확인")
+    @PostMapping("/email/check")
     public CommonResponse<String> emailAuthCodeCheck(@RequestBody AuthCheckRequestVo authCheckRequestVo) {
         authService.emailAuthCodeCheck(AuthCheckRequestDto.voToDto(authCheckRequestVo));
         return CommonResponse.success("이메일 인증코드 확인 성공");
--- src/main/java/com/nocaffeine/ssgclone/member/presentation/MemberController.java
+++ src/main/java/com/nocaffeine/ssgclone/member/presentation/MemberController.java
@@ -9,6 +9,7 @@ import com.nocaffeine.ssgclone.member.dto.response.TokenResponseDto;
 import com.nocaffeine.ssgclone.member.vo.request.*;
 import com.nocaffeine.ssgclone.member.vo.response.MemberDetailResponseVo;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -18,27 +19,28 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@Tag(name = "Member", description = "회원")
 @RequestMapping("/api/v1")
 public class MemberController {
 
     private final MemberService memberService;
     private final JwtTokenProvider jwtTokenProvider;
 
-    @Operation(summary = "이메일 중복 검증", description = "이메일 중복 검증", tags = {"Member"})
+    @Operation(summary = "이메일 중복 검증", description = "이메일 중복 검증")
     @GetMapping("/duplication")
     public CommonResponse<String> duplicationEmail(@RequestParam String email) {
         memberService.duplicationEmail(email);
         return CommonResponse.success("이메일 중복 검증 성공");
     }
 
-    @Operation(summary = "회원가입", description = "회원가입", tags = {"Member"})
+    @Operation(summary = "회원가입", description = "회원가입")
     @PostMapping("/member")
     public CommonResponse<String> memberCreate(@RequestBody MemberSaveRequestVo memberSaveRequestVo) {
         memberService.addMember(MemberSaveRequestDto.voToDto(memberSaveRequestVo));
         return CommonResponse.success("회원가입 성공");
     }
 
-    @Operation(summary = "로그인", description = "로그인", tags = {"Member"})
+    @Operation(summary = "로그인", description = "로그인")
     @PostMapping("/member/login")
     public ResponseEntity<CommonResponse<TokenResponseDto>> logIn(@RequestBody MemberLoginRequestVo memberLoginRequestVo) {
         TokenResponseDto tokenResponseDto = memberService.logIn(MemberLoginRequestDto.voToDto(memberLoginRequestVo));
@@ -47,7 +49,7 @@ public class MemberController {
                 .body(CommonResponse.success("로그인 성공"));
     }
 
-    @Operation(summary = "비밀번호 변경", description = "비밀번호 변경", tags = {"Member"})
+    @Operation(summary = "비밀번호 변경", description = "비밀번호 변경")
     @PutMapping("/member/password")
     public CommonResponse<String> changePassword(@RequestBody MemberPasswordRequestVo memberPasswordRequestVo) {
         String memberUuid = jwtTokenProvider.validateAndGetUserUuid(jwtTokenProvider.getHeader());
@@ -55,14 +57,14 @@ public class MemberController {
         return CommonResponse.success("비밀번호 변경 성공");
     }
 
-    @Operation(summary = "회원 상세 정보", description = "회원 상세 정보", tags = {"Member"})
+    @Operation(summary = "회원 상세 정보", description = "회원 상세 정보")
     @GetMapping("/member")
     public CommonResponse<MemberDetailResponseVo> memberDetail() {
         String memberUuid = jwtTokenProvider.validateAndGetUserUuid(jwtTokenProvider.getHeader());
         return CommonResponse.success("회원 상세 조회 성공",MemberDetailResponseDto.dtoToVo(memberService.findMember(memberUuid)));
     }
 
-    @Operation(summary = "회원 탈퇴", description = "회원 탈퇴", tags = {"Member"})
+    @Operation(summary = "회원 탈퇴", description = "회원 탈퇴")
     @DeleteMapping("/member")
     public CommonResponse<String> memberRemove() {
         String memberUuid = jwtTokenProvider.validateAndGetUserUuid(jwtTokenProvider.getHeader());
